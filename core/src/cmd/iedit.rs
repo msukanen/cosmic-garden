cf26880ac43ac6b86f1c87fe49cc728a37041281
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-11 17:39:55 +0300

    nab item...

--- core/src/cmd/iedit.rs
+++ core/src/cmd/iedit.rs
@@ -2,7 +2,7 @@
 
 use async_trait::async_trait;
 
-use crate::{cmd::{Command, CommandCtx}, edit::EditorMode, io::ClientState, item::container::Storage, show_help_if_needed, tell_user, validate_access};
+use crate::{cmd::{Command, CommandCtx}, edit::EditorMode, io::ClientState, item::container::Storage, player, show_help_if_needed, tell_user, validate_access};
 
 pub mod abort;
 
@@ -17,6 +17,30 @@ impl Command for IeditCommand {
             return;
         }
         show_help_if_needed!(ctx, "iedit");
+        
+        let id = ctx.args;
+        let target_item = {
+            let mut p = plr.write().await;
+            let p_lock = p.location.upgrade();
+            let found = p.inventory.take(id);
+            drop(p);
+            if found.is_none() {
+                if let Some(arc) = p_lock {
+                    arc.write().await.contents.take(id)
+                } else {
+                    None
+                }
+            } else {
+                found
+            }
+        };
 
+        if target_item.is_some() {
+            {   // tuck it into safety of iedit_buffer which will be stored on disk the moment io_thread sez so.
+                let mut w = plr.write().await;
+                w.iedit_buffer = target_item;
+            }
+            ctx.state = ClientState::Editing { player: plr.clone(), mode: EditorMode::Item { dirty: true } };
+        }
     }
 }
