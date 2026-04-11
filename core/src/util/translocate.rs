5e6f33ec8bce92a8b6184b994cc2697ba3b1ef22
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-11 16:47:36 +0300

    translocate

--- core/src/util/translocate.rs
+++ core/src/util/translocate.rs
@@ -9,20 +9,21 @@
 /// If `translocate!(a,b,c)` deadlocks, get ID earlier and use `translocate!(a,b,c,d)`.
 #[macro_export]
 macro_rules! translocate {
-    ($plr:ident, $origin:expr, $target:expr) => {
+    ($plr:expr, $origin:expr, $target:expr) => {
         {
+            use crate::identity::IdentityQuery;
             let p_id = $plr.read().await.id().to_string();
             $origin.write().await.who.remove(&p_id);
-            $target.write().await.who.insert(p_id.clone(), Arc::downgrade(&$plr));
-            $plr.write().await.location = Arc::downgrade(&$target);
+            $target.write().await.who.insert(p_id.clone(), std::sync::Arc::downgrade(&$plr));
+            $plr.write().await.location = std::sync::Arc::downgrade(&$target);
         }
     };
 
-    ($plr:ident, $p_id:ident, $origin:expr, $target:expr) => {
+    ($plr:expr, $p_id:ident, $origin:expr, $target:expr) => {
         {
             $origin.write().await.who.remove(&$p_id);
-            $target.write().await.who.insert($p_id.clone(), Arc::downgrade(&$plr));
-            $plr.write().await.location = Arc::downgrade(&$target);
+            $target.write().await.who.insert($p_id.clone(), std::sync::Arc::downgrade(&$plr));
+            $plr.write().await.location = std::sync::Arc::downgrade(&$target);
         }
     };
 }
