a0b24008f75aa084e94175319dae6be5a9872f14
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-19 09:34:12 +0300

    EntityLibrary.

---
+++ core/src/mob/spawn_lib.rs
@@ -0,0 +1,110 @@
+//! Mob/Entity blueprint library.
+
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use tokio::fs;
+
+use crate::{r#const::WORLD_ID, error::CgError, identity::IdentityQuery, io::{entity_entry_fp, entity_lib_fp}, mob::core::Entity, serial::string_vec_to_bool_map, string::StrUuid, thread::librarian::ENT_BP_LIBRARY};
+
+/// Entity (blueprint) library!
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct EntityLibrary {
+    world_id: String,
+    #[serde(with = "string_vec_to_bool_map")]
+    pub id_stem: HashMap<String, bool>,
+    #[serde(default, skip)]
+    pub bps: HashMap<String, Entity>,
+}
+
+impl EntityLibrary {
+    /// Load/bootstrap [EntityLibrary]
+    pub(crate) async fn load_or_bootstrap() -> Result<(), CgError> {
+        let Ok(mf) = fs::read_to_string(entity_lib_fp()).await else {
+            log::warn!("No entity database of any sort yet. Making (an empty) one…");
+
+            let mut lock = (*ENT_BP_LIBRARY).write().await;
+            *lock = EntityLibrary::default();
+
+            let basic_mobs_toml = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/res/bootstrap_mob.toml"));
+            #[derive(Deserialize)]
+            struct BasicMobs {
+                mobs: Vec<Entity>,
+            }
+            let wrapper: BasicMobs = toml::from_str(basic_mobs_toml)?;
+            for mob in wrapper.mobs {
+                lock.id_stem.insert(mob.id().to_string(), true);
+                lock.bps.insert(mob.id().to_string(), mob);
+            }
+            lock.save().await?;
+
+            return  Ok(())
+        };
+
+        // We got a loaded lib in `mf`
+        let mut lib: EntityLibrary = serde_json::from_str(&mf)?;
+        if lib.world_id != WORLD_ID.as_str() {
+            log::warn!("Loading entities of a different world ('{}')… Adjusting accordingly…", lib.world_id);
+        }
+        lib.world_id = WORLD_ID.as_str().into();
+        for (id, dirty) in lib.id_stem.iter_mut() {
+            let item_path = entity_entry_fp(id);
+            match fs::read_to_string(&item_path).await {
+                Ok(content) => {
+                    if let Ok(ent) = toml::from_str(&content) {
+                        lib.bps.insert(id.clone(), ent);
+                        *dirty = false;
+                    }
+                }
+
+                Err(e) => log::warn!("Failed to fetch the '{id}' from {}: {e}", item_path.display())
+            }
+        }
+
+        // mop up stems that failed to load…
+        lib.id_stem.retain(|id,_| lib.bps.contains_key(id));
+        let mut lock = (*ENT_BP_LIBRARY).write().await;
+        let count = lib.id_stem.len();
+        *lock = lib;
+        let plural = if count == 1 {"y"} else {"ies"};
+        log::info!("Entity catalogue for '{}' established with {count} entr{plural}.", WORLD_ID.as_str());
+
+        Ok(())
+    }
+
+    /// Save entity library and all the registered, modified entries.
+    pub(crate) async fn save(&mut self) -> Result<(), CgError> {
+        let contents = serde_json::to_string_pretty(&self)?;
+        fs::write(entity_lib_fp(), contents).await?;
+
+        for (id, dirty) in self.id_stem.iter_mut() {
+            if !*dirty { continue; }
+
+            let Some(mob) = self.bps.get(id) else {
+                log::error!("Could not find '{id}' in 'bps'");
+                continue;
+            };
+
+            if let Ok(_) = mob.save_bp().await {
+                *dirty = false;
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Get an [Entity] blueprint from cold storage, if exists.
+    pub fn get(&self, id: &str) -> Option<Entity> {
+        self.bps.get(id.show_uuid(false)).cloned()
+    }
+}
+
+impl Default for EntityLibrary {
+    fn default() -> Self {
+        EntityLibrary {
+            world_id: WORLD_ID.as_str().to_string(),
+            id_stem: HashMap::new(),
+            bps: HashMap::new()
+        }
+    }
+}
