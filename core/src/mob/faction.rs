e0d0329228ae219d7dc7bafcc68aee07cc2148d8
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-27 17:00:49 +0300

    Display for Faction.

--- core/src/mob/faction.rs
+++ core/src/mob/faction.rs
@@ -1,5 +1,7 @@
 //! Mobs have 'factions' (Players too…).
 
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
 /// Some basic/preliminary factions.
@@ -19,4 +21,16 @@ pub trait Factioned {
 
 pub trait FactionMut {
     fn faction_mut(&mut self) -> &mut EntityFaction;
+}
+
+impl Display for EntityFaction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Friendly => write!(f, "friendly"),
+            Self::Neutral => write!(f, "neutral"),
+            Self::Hostile => write!(f, "hostile"),
+            Self::Guard => write!(f, "guard"),
+            Self::Player { pvp } => write!(f, "player{}", if *pvp {" (PvP)"} else {""}),
+        }
+    }
 }
