7e1c9ee7d1473fbb9fb64d4e93587e1edaa4e8c8
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-28 00:16:04 +0300

    moved Tickable impl for Entity under core.rs.

--- core/src/traits/tickable.rs
+++ core/src/traits/tickable.rs
@@ -2,22 +2,9 @@
 
 use async_trait::async_trait;
 
-use crate::{combat::CombatantMut, mob::core::Entity};
-
 #[async_trait]
 pub trait Tickable {
     async fn tick(&mut self) -> bool;
-}
-
-#[async_trait]
-impl Tickable for Entity {
-    async fn tick(&mut self) -> bool {
-        let hp = self.hp_mut().tick().await;
-        let mp = self.mp_mut().tick().await;
-        let sn = self.sn_mut().tick().await;
-        let san = self.san_mut().tick().await;
-        hp || mp || sn || san
-    }
 }
 
 /// - "What it means, what it means?"
