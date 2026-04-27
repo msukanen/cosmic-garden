db203d1a5ca8ab949ee819aa09e05c471c61bc1e
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-28 00:17:20 +0300

    faction display, try_from.

--- core/src/mob/faction.rs
+++ core/src/mob/faction.rs
@@ -15,6 +15,22 @@ pub enum EntityFaction {
     Hostile,
 }
 
+pub enum EntityFactionError {
+    VoidFaction,
+    NotFaction(String),
+    CannotSetPlayerPVP,
+}
+
+impl Display for EntityFactionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::CannotSetPlayerPVP => write!(f, "Only player can set themselves into Hardcore/PvP mode."),
+            Self::NotFaction(x) => write!(f, "'{x}' isn't a recognized \"faction\"."),
+            Self::VoidFaction => write!(f, "An empty string doesn't represent any faction whatsoever…"),
+        }
+    }
+}
+
 pub trait Factioned {
     fn faction(&self) -> EntityFaction;
 }
@@ -32,5 +48,21 @@ impl Display for EntityFaction {
             Self::Guard => write!(f, "guard"),
             Self::Player { pvp } => write!(f, "player{}", if *pvp {" (PvP)"} else {""}),
         }
+    }
+}
+
+impl TryFrom<&str> for EntityFaction {
+    type Error = EntityFactionError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.is_empty() { return Err(EntityFactionError::VoidFaction) }
+        Ok(match value.chars().nth(0).unwrap() {
+            'f'|'F' => Self::Friendly,
+            'n'|'N' => Self::Neutral,
+            'g'|'G' => Self::Guard,
+            'h'|'H'|
+            'a'|'A' => Self::Hostile,
+            'p' => return Err(EntityFactionError::CannotSetPlayerPVP),
+            _ => return Err(EntityFactionError::NotFaction(value.into()))
+        })
     }
 }
