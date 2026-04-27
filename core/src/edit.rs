714342b0597dce8a16c1175e50fad8e23392890d
Markku Sukanen <markku.sukanen@gmail.com>
2026-04-27 11:03:45 +0300

    validate_editor_mode!(..) to bail out from editor sub-commands in case something wonky happens with command routing...

--- core/src/edit.rs
+++ core/src/edit.rs
@@ -51,3 +51,12 @@ impl EditorMode {
         }
     }
 }
+
+#[macro_export]
+macro_rules! validate_editor_mode {
+    ($ctx:ident, $mode:literal) => {{
+        if !$ctx.state.is_editing() {
+            crate::err_tell_user!($ctx.writer, "Invoke {} first…\n", $mode);
+        }
+    }};
+}
