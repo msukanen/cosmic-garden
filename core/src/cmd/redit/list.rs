13b5ba3d45664fde8099025b4b63e1799a5e77ab
Markku Sukanen <markku.sukanen@gmail.com>
2026-05-05 20:50:49 +0300

    testing room listing...

--- core/src/cmd/redit/list.rs
+++ core/src/cmd/redit/list.rs
@@ -2,66 +2,74 @@
 
 use async_trait::async_trait;
 
-use crate::{cmd::{Command, CommandCtx}, identity::IdentityQuery, tell_user, validate_access};
+use crate::{cmd::{Command, CommandCtx}, identity::IdentityQuery, string::styling::MAX_DESCRIPTION_LINES, tell_user, validate_access, world::room_list};
 
 pub struct ListCommand;
 
 #[async_trait]
 impl Command for ListCommand {
     async fn exec(&self, ctx: &mut CommandCtx<'_>) {
+        const PAGE_COLS: usize = 1;
+        const ENT_PER_PAGE: usize = MAX_DESCRIPTION_LINES * PAGE_COLS;
+
         let _ = validate_access!(ctx, builder);
-        let mut report = String::from("<c green>--- The Existing Reality ---</c>\n");
         let (what, page) = ctx.args.split_once(' ').unwrap_or((ctx.args, "1"));
-        let Ok(mut page) = page.parse::<usize>() else {
-            tell_user!(ctx.writer, "*cough* '{}' is not a valid number, by the by…\n", page);
-            return ;
+        let mut page_num: usize;
+        let term = if let Ok(num_what) = what.parse::<usize>() {
+            page_num = num_what;
+            None
+        } else {
+            let Ok(page) = page.parse::<usize>() else {
+                tell_user!(ctx.writer, "*cough* '{}' is not a valid number, by the by…\n", page);
+                return;
+            };
+            page_num = page;
+            Some(what.to_string())
         };
-        if page == 0 {
-            page = 1;
-        }
-        let rooms = {
-            let w = ctx.world.read().await;
-            let rooms = w.paginated_room_entries(what, page, 10).await;
-            for (m_id, arc) in &rooms.entries {
+
+        let mut report = String::from("<c green>--- The Existing Reality ---</c>\n");
+        let (total_found, total_pages) = {
+            let mut rooms = room_list(&ctx.world, term).await;
+            let r_len = rooms.len();
+            let total_pages = (r_len + (ENT_PER_PAGE - 1)) / ENT_PER_PAGE;
+            page_num = page_num.min(total_pages).saturating_sub(1);
+            let mut entry = 0;
+            let total_found = rooms.len();
+            if page_num > 0 {
+                rooms.drain(0..(ENT_PER_PAGE * page_num) - 1);
+            };
+            for (m_id, arc) in &rooms {
                 let lock = arc.read().await;
-                report.push_str(&format!("  <c yellow>{}</c> <c gray>:</c> {} <c gray>:</c> <c cyan>{}</c>\n", m_id, lock.id(), lock.title()));
+                report.push_str(&format!("  <c yellow>{:>20}</c> <c gray>:</c> {} <c gray>:</c> <c cyan>{}</c>\n", m_id, lock.id(), lock.title()));
+                entry += 1;
+                if entry >= ENT_PER_PAGE { break; }
             }
-            rooms
+            (total_found, total_pages)
         };
-        tell_user!(ctx.writer, "{}\nTotal: {} matches ({page} of {} pages).\n", report, rooms.total_found, rooms.total_pages);
+        tell_user!(ctx.writer, "{report}\nTotal: {} matches ({} of {} pages).\n", total_found, page_num+1, total_pages);
     }
 }
 
 #[cfg(test)]
 mod cmd_redit_list {
-    use dicebag::InclusiveRandomRange;
-
-    use crate::{cmd::{look::LookCommand, redit::{ReditCommand, list::ListCommand}}, ctx, room::Room, util::access::Access, world::world_tests::get_operational_mock_world};
+    use crate::{cmd::{redit::{ReditCommand, list::ListCommand}}, ctx, room::Room, util::access::Access, world::world_tests::get_operational_mock_world};
 
     #[tokio::test]
     async fn exits_listing() {
         let mut b: Vec<u8> = Vec::new();
         let mut s = std::io::Cursor::new(&mut b);
-        let (w,c,(state, p),_) = get_operational_mock_world().await;
-        let state = ctx!(state, ReditCommand, "this", s, c.out, w,|out:&str| out.contains("Huh?"));
+        let (w,c,(mut state, p),_) = get_operational_mock_world().await;
+        let c = c.out;
+        state = ctx!(state, ReditCommand, "this", s,c,w,|out:&str| out.contains("Huh?"));
         p.write().await.access = Access::Builder;
-        let state = ctx!(state, ReditCommand, "this", s, c.out, w);
-        let state = ctx!(state, LookCommand, "", s,c.out,w, |out:&str| out.contains("***"));
-        let _ = ctx!(state, ListCommand, "", s,c.out,w, |out:&str| out.contains("r-1") && out.contains("r-2"));
-    }
-
-    #[tokio::test]
-    async fn exits_listing_parallel() {
-        let mut b: Vec<u8> = Vec::new();
-        let mut s = std::io::Cursor::new(&mut b);
-        let (w,c,(state, p),_) = get_operational_mock_world().await;
-        let state = ctx!(state, ReditCommand, "this", s, c.out, w,|out:&str| out.contains("Huh?"));
-        p.write().await.access = Access::Builder;
-        let state = ctx!(state, ReditCommand, "this", s, c.out, w);
-        let state = ctx!(state, ListCommand, "", s,c.out,w, |out:&str| out.contains("Waterfall") && out.contains("Incineration"));
+        state = ctx!(state, ReditCommand, "this", s,c,w);
+        state = ctx!(state, ListCommand, "", s,c,w,|out:&str| out.contains("Waterfall") && out.contains("Incineration"));
         let mut lock = w.write().await;
-        for i in 3..=1_000 {
-            let id = format!("{}-{i}", ('a'..='z').random_of());
+        
+        let alphabet: Vec<char> = ('a'..='z').collect();
+        let alphalen = alphabet.len();
+        for i in 1..=1_000 {
+            let id = format!("{}-{i}", alphabet[i % alphalen]);
             let t = format!("Room #{i}");
             let d = format!("This would be the room #{i}");
             let r = Room::new(&id, &t).await.ok().unwrap();
@@ -69,7 +77,7 @@ mod cmd_redit_list {
             lock.insert_room(r).await.ok();
         }
         drop(lock);
-        let _ = ctx!(state, ListCommand, "r-1 0", s,c.out,w, |out:&str| out.contains("Inciner"));
+        state = ctx!(state, ListCommand, "40", s,c,w,|out:&str| out.contains("matches (40"));
+        state = ctx!(state, ListCommand, "^o-40$", s,c,w,|out:&str| out.contains("Room #40") && out.contains("Total: 1 m") && !out.contains("#404"));
     }
-
 }
