b1bb88c1bded519bfcc5c5dcca57f783be7997e2
Markku Sukanen <markku.sukanen@gmail.com>
2026-05-09 19:53:33 +0300

    more DRY modularity to translocate!(..).

--- core/src/util/translocate.rs
+++ core/src/util/translocate.rs
@@ -16,8 +16,7 @@ macro_rules! translocate {
             use crate::identity::IdentityQuery;
             let p_id = $plr.read().await.id().to_string();
             $origin.write().await.who.remove(&p_id);
-            $target.write().await.who.insert(p_id.clone(), std::sync::Arc::downgrade(&$plr));
-            $plr.write().await.set_location(&$target).await;
+            crate::translocate!(already_removed; p_id, $plr, $target);
         }
     };
 
@@ -25,16 +24,32 @@ macro_rules! translocate {
     ($plr:expr, $p_id:ident, $origin:expr, $target:expr) => {
         {
             $origin.write().await.who.remove(&$p_id);
+            crate::translocate!(already_removed; $p_id, $plr, $target);
+        }
+    };
+
+    // Translocate [$plr][Player] from [$origin][Room] to [$target][Room].
+    // They've been already drained/removed from $origin.
+    (already_removed; $p_id:ident, $plr:expr, $target:expr) => {
+        {
             $target.write().await.who.insert($p_id.clone(), std::sync::Arc::downgrade(&$plr));
-            $plr.write().await.set_location(&$target).await;
+            $plr.write().await.set_location(&$target).await.ok();
         }
     };
 
     // Translocate [$ent][Entity] (ID $id) from [$origin][Room] to [$target][Room]
-    (ent $ent:expr, $id:ident, $origin:expr, $target:expr) => {
+    (ent $ent:ident, $id:ident, $origin:expr, $target:expr) => {
+        {
+            $origin.write().await.entities.remove(&$id);
+            crate::translocate!(ent $ent, $id, $target);
+        }
+    };
+
+    // Translocate [$ent][Entity] (ID $id) to [$target][Room].
+    // They have already been removed from origin before translocate!() call.
+    (ent $ent:ident, $id:ident, $target:expr) => {
         {
             use crate::combat::CombatantMut;
-            $origin.write().await.entities.remove(&$id);
             $target.write().await.entities.insert($id.clone(), $ent.clone());
             *($ent.write().await.location_mut()) = std::sync::Arc::downgrade(&$target);
         }
