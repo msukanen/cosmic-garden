ddfe183f22e53b366bb21b57fa3701267c0edef2
Markku Sukanen <markku.sukanen@gmail.com>
2026-05-10 12:30:48 +0300

    refactor-combine Sanitizer under 'str' umbrella.

--- core/src/string/sanitize.rs
+++ core/src/string/sanitize.rs
@@ -4,19 +4,11 @@ pub trait Sanitizer {
     fn sanitize(&self) -> String;
 }
 
-impl Sanitizer for &str {
+impl Sanitizer for str {
     /// Sanitize away all "control" characters.
     fn sanitize(&self) -> String {
         self.trim().chars()
             .filter(|c| !c.is_control())
             .collect::<String>()
     }
-}
-
-impl Sanitizer for String {
-    fn sanitize(&self) -> String { self.as_str().sanitize()}
-}
-
-impl Sanitizer for &String {
-    fn sanitize(&self) -> String { self.as_str().sanitize()}
 }
