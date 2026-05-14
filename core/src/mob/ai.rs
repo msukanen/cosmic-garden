a4b56bfed11d43645eb61e244654b642d88c6f8f
Markku Sukanen <markku.sukanen@gmail.com>
2026-05-15 00:13:05 +0300

    AI mental state.

--- core/src/mob/ai.rs
+++ core/src/mob/ai.rs
@@ -2,7 +2,7 @@
 
 use serde::{Deserialize, Serialize};
 
-use crate::{room::environ::{SpecialEnvironment, Terrain, WEATHER_RAIN}, single_tick_meaning, traits::TickMeaning};
+use crate::{identity::MachineId, room::environ::{SpecialEnvironment, Terrain, WEATHER_RAIN}, single_tick_meaning, traits::TickMeaning};
 
 #[derive(Debug, Clone, Copy, Deserialize, Serialize, PartialEq)]
 pub enum AiState {
@@ -32,7 +32,7 @@ impl Default for AiMentalState {
 
 #[derive(Debug, Clone)]
 pub enum AiAction {
-    Emote { fmt: &'static str },
+    Emote { ent_m_id: MachineId, fmt: &'static str },
     //Attack { xyz }
 }
 
@@ -47,12 +47,17 @@ pub struct Ai {
     p_wandering_to_idle: f32,
 }
 
-fn ai_rng_default() -> u64 {
+pub(crate) fn ai_rng_default() -> u64 {
     rand::random::<u64>()
 }
 
+/// `seed` the "RNG"; return the new value.
 #[inline] fn ai_rng(seed: u64) -> u64 { seed.wrapping_mul(6364136223846793005).wrapping_add(1) }
+/// Generate a [0.0, 1.0] range value from give `base`.
+// Perfect 1.0 is rare, but… can happen, thus the range isn't [0.0, 1.0)
 #[inline] const fn ai_probability(base: u64) -> f32 { (base >> 32) as f32 / 4294967296.0 }
+/// Check vs [0.0, 1.0] range whether to do something…
+#[inline] const fn ai_do(base: u64, chance: f32) -> bool { ai_probability(base) <= chance }
 
 impl Default for Ai {
     fn default() -> Self {
@@ -70,15 +75,17 @@ impl Default for Ai {
 impl Ai {
     /// Tick the AI.
     /// 
-    // By default we run AI at some fraction of the parent's [Room]'s Hz.
-    pub fn tick(&mut self, /* e_title: &str, */ curr_tick: usize, room_env: SpecialEnvironment, room_terrain: Option<Terrain>) -> Option<TickMeaning> {
+    // By default we (usually) run AI at some fraction of the parent's [Room]'s Hz.
+    pub fn tick(&mut self, e_tick_id: MachineId, curr_tick: usize, room_env: SpecialEnvironment, room_terrain: Option<Terrain>) -> Option<TickMeaning> {
         let mut maybe_state = None;
         let mut maybe_mental_state = None;
         let mut maybe_action = None;
+        
+        // Idle → Wander → Idle switcharoo.
         self.rng = ai_rng(self.rng);
         match self.state {
             AiState::Idle => {
-                if ai_probability(self.rng) <= 0.01 {
+                if ai_do(self.rng, 0.01) {
                     self.state = AiState::Wandering;
                     maybe_state = self.state.into();
                 }
@@ -86,30 +93,54 @@ impl Ai {
 
             AiState::Wandering => {
                 // move?
-                if ai_probability(self.rng) <= 0.05 {
+                if ai_do(self.rng, 0.05) {
                     self.state = AiState::Idle;
                     maybe_state = self.state.into();
                 }
             }
         }
 
+        self.rng = ai_rng(self.rng);
+        // Some weather related checks…
         if room_env & WEATHER_RAIN != 0 {
             match self.mental_state {
-                AiMentalState::Grumpy => {
-                    // let's not spam too much…
-                    self.rng = ai_rng(self.rng);
-                    if ai_probability(self.rng) <= 0.01 {
-                        maybe_action = AiAction::Emote { fmt: "[~e~] glares at the clouds for a moment." }.into()
+                AiMentalState::Grumpy =>
+                    if ai_do(self.rng, 0.005) {
+                        maybe_action = AiAction::Emote { ent_m_id: e_tick_id, fmt: "[~e~] glares at the clouds for a moment." }.into()
                     }
-                },
-                AiMentalState::Neutral => {
-                    self.rng = ai_rng(self.rng);
-                    if ai_probability(self.rng) <= 20.0 {
+                ,
+                AiMentalState::Neutral =>
+                    if ai_do(self.rng, 0.2) {
                         self.mental_state = AiMentalState::Grumpy;
                         maybe_mental_state = self.mental_state.into();
                     }
+                ,
+                AiMentalState::Happy => {
+                    if ai_do(self.rng, 0.05) {
+                        self.mental_state = AiMentalState::Neutral;
+                        maybe_mental_state = self.mental_state.into();
+                    }
                 }
-                _ => () // a bit of rain doesn't make Happy worse nor Angry any angrier…
+                // a bit of rain doesn't make Angry any angrier…
+                _ => ()
+            }
+        } else {
+            match self.mental_state {
+                AiMentalState::Grumpy => {
+                    if ai_do(self.rng, 1.0/3.0) {
+                        self.mental_state = AiMentalState::Neutral;
+                        maybe_mental_state = self.mental_state.into();
+                    }
+                }
+
+                AiMentalState::Neutral => {
+                    if ai_do(self.rng, 0.02) {
+                        self.mental_state = AiMentalState::Happy;
+                        maybe_mental_state = self.mental_state.into();
+                    }
+                }
+                // no change for Angry
+                _ => ()
             }
         }
         
