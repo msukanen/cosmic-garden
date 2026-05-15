0edbd7a895ebb7297e2772824c188f71be5f65bf
Markku Sukanen <markku.sukanen@gmail.com>
2026-05-15 16:45:46 +0300

    mem_as_gmk  const fn.

--- core/src/util.rs
+++ core/src/util.rs
@@ -4,8 +4,15 @@ pub mod activity;
 pub mod approx;
 pub mod config;
 pub mod direction;
-pub mod ed;
 pub(crate) mod escape_hatch;
 //pub mod intentqueue; pub use intentqueue::*;
 pub mod translocate;
 pub mod volume; pub use volume::{Volumed, VolumeMut};
+
+#[inline]
+pub const fn mem_as_gmk(bytes: f64) -> (f64, f64, f64) {
+    let kib = bytes as f64 / 1024.0;
+    let mib = kib / 1024.0;
+    let gib = mib / 1024.0;
+    (kib, mib, gib)
+}
