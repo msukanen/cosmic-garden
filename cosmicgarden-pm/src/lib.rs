ad8c9cc3e7d0a671563dfa8335a1e295c39c3033
Markku Sukanen <markku.sukanen@gmail.com>
2026-08-14 22:10:32 +0300

    get_struct_fields etc.

--- cosmicgarden-pm/src/lib.rs
+++ cosmicgarden-pm/src/lib.rs
@@ -57,6 +57,13 @@ macro_rules! get_tagged_ident {
 }
 
 macro_rules! req_field {
+    (named $data:ident, $field:literal) => {
+        $data.named.iter().find(|f| {
+            f.ident.as_ref().map_or(false, |i| i == $field)
+        })  .map(|f| f.ident.as_ref().unwrap())
+            .expect(&format!("No '{}' field found in #name", $field))
+    };
+
     ($data:ident, $field:literal) => {
         $data.fields.iter().find(|f| {
             f.ident.as_ref().map_or(false, |i| i == $field)
@@ -66,11 +73,28 @@ macro_rules! req_field {
 }
 
 macro_rules! maybe_field {
+    (named $data:ident, $field:literal) => {
+        $data.named.iter().find(|f| {
+            f.ident.as_ref().map_or(false, |i| i == $field)
+        })  .map(|f| f.ident.as_ref().unwrap())
+    };
+
     ($data:ident, $field:literal) => {
         $data.fields.iter().find(|f| {
             f.ident.as_ref().map_or(false, |i| i == $field)
         })  .map(|f| f.ident.as_ref().unwrap())
     };
+}
+
+fn get_struct_fields(input: &DeriveInput) -> &syn::FieldsNamed {
+    match &input.data {
+        Data::Struct(data) => match &data.fields {
+            syn::Fields::Named(fields) => fields,
+            _ => unimplemented!("Only named fields supported.")
+        },
+
+        _ => unimplemented!("Only structs supported.")
+    }
 }
 
 /// Generate read-only [IdentityQuery] variant's internals.
